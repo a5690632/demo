a763248e2f3f4d67c3d8249a06c0b6212d29d4d3
baishipeng <215200924@qq.com>
2019-02-26 11:15:18 +0800

    mysql联系

--- node/express.js
+++ node/express.js
@@ -1,41 +1,49 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const login = require("./express1");
 
 //获取cookie的中间件
-var cookie = require('cookie-parser');
-app.use(cookie())
+var cookie = require("cookie-parser");
+app.use(cookie());
 
 //静态资源
-app.use(express.static("www"))
+app.use(express.static("www"));
 //获取post请求的数据 res.body
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({
-    extended: false
-}))
-app.use(bodyParser.json())
+const bodyParser = require("body-parser");
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+app.use(bodyParser.json());
 //session
-let session = require("cookie-session")
-app.use(session({
-    keys: ["123", "asda"]
-}))
+// let session = require("cookie-session");
+// app.use(
+//     session({
+//         keys: ["123", "asda"]
+//     })
+// );
 
-
-app.all('*', function (req, res, next) {
+app.all("*", function(req, res, next) {
     //跨域处理
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header("Access-Control-Allow-Headers",
-        "X-Requested-With,Origin,Content-Type,Accept");
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-    if (req.method == 'OPTIONS') {
-        res.sendStatus(200)
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,Origin,Content-Type,Accept"
+    );
+    res.header(
+        "Access-Control-Allow-Methods",
+        "PUT, POST, GET, DELETE, OPTIONS"
+    );
+    if (req.method == "OPTIONS") {
+        res.sendStatus(200);
     } else {
         next();
     }
 });
-app.use("/", (req, res) => {
+app.use("/", (req, res, next) => {
     // req.secure="密钥"
     // 只能用cookie获取
-
     // 用cookie-parser 设置的签名cookie
     //用  req.signedCookies 获取
     // res.cookie('user', 1, {
@@ -43,15 +51,16 @@ app.use("/", (req, res) => {
     // })
     //加密
     // res.clearCookie() 删除cookie
-    
     // 获取
-    if (req.session["user"]) {
-        req.session["user"]++
-    } else {
-        req.session["user"] = 1 //设置
-    }
-    delete req.session["user"] //删除
+    // if (req.session["user"]) {
+    //     req.session["user"]++;
+    // } else {
+    //     req.session["user"] = 1; //设置
+    // }
+    // delete req.session["user"]; //删除
+    // res.sendStatus(200); S
 
-    res.sendStatus(200)
-})
-app.listen(99)
+    next();
+});
+app.use("/login", login);
+app.listen(99);
--- node/express1.js
+++ node/express1.js
@@ -1,57 +1,17 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const mysql = require("mysql");
 
-//获取cookie的中间件
-var cookie = require('cookie-parser');
-app.use(cookie())
-
-//静态资源
-app.use(express.static("www"))
-//获取post请求的数据 res.body
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({
-    extended: false
-}))
-app.use(bodyParser.json())
-//session
-let session = require("cookie-session")
-app.use(session({
-    keys: ["123", "asda"]
-}))
-
-
-app.all('*', function (req, res, next) {
-    //跨域处理
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header("Access-Control-Allow-Headers",
-        "X-Requested-With,Origin,Content-Type,Accept");
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-    if (req.method == 'OPTIONS') {
-        res.sendStatus(200)
-    } else {
-        next();
-    }
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "baishipeng",
+    database: "user"
 });
-app.use("/", (req, res) => {
-    // req.secure="密钥"
-    // 只能用cookie获取
-
-    // 用cookie-parser 设置的签名cookie
-    //用  req.signedCookies 获取
-    // res.cookie('user', 1, {
-    //     signed: true
-    // })
-    //加密
-    // res.clearCookie() 删除cookie
-    
-    // 获取
-    if (req.session["user"]) {
-        req.session["user"]++
-    } else {
-        req.session["user"] = 1 //设置
-    }
-    delete req.session["user"] //删除
-
-    res.sendStatus(200)
-})
-app.listen(99)
+db.query("SELECT * FROM `user`", (err, data) => {
+    console.log(data);
+});
+const router = express.Router();
+router.post("/", () => {
+    console.log(2);
+});
+module.exports = router;
